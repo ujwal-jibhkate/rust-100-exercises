a8435ba543698be709e4dab116ba127a1e4eaad2
Ujwal Jibhkate <ujwaljibhkate@gmail.com>
2025-09-15 16:18:57 -0400

    chapter 1 codes

--- exercises/01_intro/01_syntax/src/lib.rs
+++ exercises/01_intro/01_syntax/src/lib.rs
@@ -3,7 +3,7 @@
 //  partner in this course and it'll often guide you in the right direction!
 //
 // The input parameters should have the same type of the return type.
-fn compute(a, b) -> u32 {
+fn compute(a: i32, b: i32) -> u32 {
     // Don't touch the function body.
     a + b * 2
 }
