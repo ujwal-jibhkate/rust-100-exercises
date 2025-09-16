9949f1dde6845150d716d7e7e87371234827faa8
Ujwal Jibhkate <ujwaljibhkate@gmail.com>
2025-09-16 14:15:29 -0400

    Chapter 2 : Solution 7 and 8

--- exercises/02_basic_calculator/07_for/src/lib.rs
+++ exercises/02_basic_calculator/07_for/src/lib.rs
@@ -1,6 +1,12 @@
 // Rewrite the factorial function using a `for` loop.
-pub fn factorial(n: u32) -> u32 {
-    todo!()
+pub fn factorial( n: u32) -> u32 {
+    let mut sum:u32 = 1;
+
+    for i in 1..=n {
+        sum = sum * i
+
+    }
+    return sum
 }
 
 #[cfg(test)]
